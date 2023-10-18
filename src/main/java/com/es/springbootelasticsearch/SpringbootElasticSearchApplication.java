a86be670607bf665268c87e75e5e56e49f336d88
william <2644985344@qq.com>
2023-10-19 01:04:13 +0800

    springboot2.7集成elasticsearch8.x Demo

---
+++ src/main/java/com/es/springbootelasticsearch/SpringbootElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.es.springbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/es/springbootelasticsearch/SpringbootElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.springbootelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
